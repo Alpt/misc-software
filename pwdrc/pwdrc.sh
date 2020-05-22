ae835f5b21f12e538b82213d00e2cb2f57350718
andrea <andrea@email.domain>
2020-05-23 00:43:33 +0200

    modified:   pwdrc.sh
    code more readble
    removed http links

--- pwdrc/pwdrc.sh
+++ pwdrc/pwdrc.sh
@@ -14,13 +14,13 @@
 
 IAM=`whoami`
 alias_file=".$IAM""_pwdrc"
-PWDRC_RECURSE="0"
-PWDRC_RECURSE_INDEX="0"
+pwdrc_recurse="0"
+pwdrc_recurse_idx="0"
 
 pwdrc_exec_recurse_function() {
 	local ii
 
-	for ((ii=1; ii <= $PWDRC_RECURSE_INDEX; ii++))
+	for ((ii=1; ii <= $pwdrc_recurse_idx; ii++))
 	do
 		if [ "${PWDRC_REC_FUNC_ARRAY[$ii]}" != 0 ]
 		then
@@ -33,47 +33,48 @@ pwdrc_exec_recurse_function() {
 }
 
 pwdrc_cdd() {
-	local dir_upper_changed
+	local still_in_subdir
 
 	PWD_NOW=$PWD
 	
 	[ -z "$1" ] && [ -z "$HOME" ] && cd /
-	[ -z "$1" ] && cd "$HOME"
+	[ -z "$1" ] && [ "$HOME" ] && cd "$HOME"
 	
 	if ! [ -d "$1" -a -x "$1" ]
 	then
+        # fallback to cd
 		cd "$1"
 		return $?
 	fi
+
 	cd "$1"
 
 	# Check if we walked into a subdir or not
-	echo `pwd` | grep -q "^${PWDRC_OLD_PWD[$PWDRC_RECURSE_INDEX]}"
-	dir_upper_changed=$?
+	echo $PWD | grep -q "^${pwdrc_old_pwd[$pwdrc_recurse_idx]}"
+	still_in_subdir=$?
 
-	if [ "$dir_upper_changed" == 0 ]
+	if [ "$still_in_subdir" == 0 ]
 	then
 		pwdrc_exec_recurse_function
-	elif [ "${PWDRC_RECURSE_ARRAY[$PWDRC_RECURSE_INDEX]}" == 0 -o\
-		"$dir_upper_changed" == 1 ]
+	elif [ "${pwdrc_recurse_array[$pwdrc_recurse_idx]}" == 0 ]
 	then
-		if [ "${PWDRC_CLOSE_ARRAY[$PWDRC_RECURSE_INDEX]}" != "0" ]
+		if [ "${pwdrc_close_array[$pwdrc_recurse_idx]}" != "0" ]
 		then
 			# execute close_pwdrc()
-			eval "${PWDRC_CLOSE_ARRAY[$PWDRC_RECURSE_INDEX]}"
+			eval "${pwdrc_close_array[$pwdrc_recurse_idx]}"
 			close_pwdrc
 			unset close_pwdrc
-			PWDRC_RECURSE="0"
-			PWDRC_CLOSE_ARRAY[$PWDRC_RECURSE_INDEX]="0"
+			pwdrc_recurse="0"
+			pwdrc_close_array[$pwdrc_recurse_idx]="0"
 		fi
 
-		if [ "$dir_upper_changed" == 1 ]
+		if [ "$still_in_subdir" == 1 ]
 		then
-			PWDRC_OLD_PWD[$PWDRC_RECURSE_INDEX]=""
-			PWDRC_RECURSE_ARRAY[$PWDRC_RECURSE_INDEX]=""
-			PWDRC_CLOSE_ARRAY[$PWDRC_RECURSE_INDEX]=""
-			PWDRC_REC_FUNC_ARRAY[$PWDRC_RECURSE_INDEX]=""
-			((PWDRC_RECURSE_INDEX--))
+			pwdrc_old_pwd[$pwdrc_recurse_idx]=""
+			pwdrc_recurse_array[$pwdrc_recurse_idx]=""
+			pwdrc_close_array[$pwdrc_recurse_idx]=""
+			PWDRC_REC_FUNC_ARRAY[$pwdrc_recurse_idx]=""
+			((pwdrc_recurse_idx--))
 
 			pwdrc_exec_recurse_function
 		fi
@@ -87,34 +88,40 @@ pwdrc_cdd() {
 			"$user_perm" == "$IAM 500" -o\
 			"$user_perm" == "$IAM 400" ]
 		then	
-			unset PWDRC_RECURSE
+
+			unset pwdrc_recurse
+            
+            # source the pwdrc script in the current directory
 			. $alias_file
-			[ -z "$PWDRC_RECURSE" ] && PWDRC_RECURSE="0";
+
+			[ -z "$pwdrc_recurse" ] && pwdrc_recurse="0";
 		
-			if [ "$PWD" != "${PWDRC_OLD_PWD[$PWDRC_RECURSE_INDEX]}" ]
+			if [ "$PWD" != "${pwdrc_old_pwd[$pwdrc_recurse_idx]}" ]
 			then
-				((PWDRC_RECURSE_INDEX++))
-				PWDRC_OLD_PWD[$PWDRC_RECURSE_INDEX]="`pwd`"
-				PWDRC_RECURSE_ARRAY[$PWDRC_RECURSE_INDEX]="$PWDRC_RECURSE"
+				((pwdrc_recurse_idx++))
+				pwdrc_old_pwd[$pwdrc_recurse_idx]="$PWD"
+				pwdrc_recurse_array[$pwdrc_recurse_idx]="$pwdrc_recurse"
 
 				if declare -f close_pwdrc > /dev/null
 				then
-					PWDRC_CLOSE_ARRAY[$PWDRC_RECURSE_INDEX]="`declare -f close_pwdrc`"
+					pwdrc_close_array[$pwdrc_recurse_idx]="`declare -f close_pwdrc`"
 				else
-					PWDRC_CLOSE_ARRAY[$PWDRC_RECURSE_INDEX]="0"
+					pwdrc_close_array[$pwdrc_recurse_idx]="0"
 				fi
 
 				if declare -f recurse_pwdrc > /dev/null
 				then
-					PWDRC_REC_FUNC_ARRAY[$PWDRC_RECURSE_INDEX]="`declare -f recurse_pwdrc`"
+					PWDRC_REC_FUNC_ARRAY[$pwdrc_recurse_idx]="`declare -f recurse_pwdrc`"
 					# execute recurse_pwdrc()
 					recurse_pwdrc
 					unset recurse_pwdrc
 				else
-					PWDRC_REC_FUNC_ARRAY[$PWDRC_RECURSE_INDEX]="0"
+					PWDRC_REC_FUNC_ARRAY[$pwdrc_recurse_idx]="0"
 				fi
 			fi
-		fi
+        else
+            echo Warning: wrong permission on "$alias_file". Group and Others must have 0 permissions. For example, do chmod 600 $(printf '"%s"' "$alias_file").
+        fi
 	fi
 }
 
